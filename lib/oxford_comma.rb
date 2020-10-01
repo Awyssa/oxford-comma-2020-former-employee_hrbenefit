a62a43cf16ce9f577bf2ee83bf03bba4ec1666e5
Michael Ernest <m.hendersun@gmail.com>
2020-10-01 19:16:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array == [2]
     array.join("and")
-  elsif
-    array.join("and ")
+  elsif array > 2
+    array.join(array[-1], "and")
   end
   array.join(", ")
 end
